abd2b0dda75e0f3bbfcd9c02aef3262590d36ea6
djay.yim <angelsocer@gmail.com>
2022-04-20 20:53:26 +0900

    init

---
+++ src/main/java/com/angelsocer/optionalclazz/OptionalClazzApplication.java
@@ -0,0 +1,13 @@
+package com.angelsocer.optionalclazz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptionalClazzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OptionalClazzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/angelsocer/optionalclazz/OptionalClazzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angelsocer.optionalclazz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptionalClazzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
